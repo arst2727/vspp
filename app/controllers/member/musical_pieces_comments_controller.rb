327e6472c941b88fecaea0acca9d4fbfef041c31
arst2727 <yanagit4729@gmail.com>
2021-09-09 10:15:25 +0000

    comment off

--- app/controllers/member/musical_pieces_comments_controller.rb
+++ app/controllers/member/musical_pieces_comments_controller.rb
@@ -1,20 +1,20 @@
 class Member::MusicalPiecesCommentsController <
-  def create
-    musical_piece = MusicalPieceComment.find(params[:musical_piece_id])
-    comment = current_user.musical_piece_comments.new(musical_piece_comment_params)
-    comment.musical_piece_id = musical_piece.id
-    if comment.save
-      redirect_back(fallback_location: root_path)
-    else
-      flash[:alert] = "comment can't be blank<br>maximum is 200 characters"
-      redirect_back(fallback_location: root_path)
-    end
-  end
+  # def create
+  #   musical_piece = MusicalPieceComment.find(params[:musical_piece_id])
+  #   comment = current_user.musical_piece_comments.new(musical_piece_comment_params)
+  #   comment.musical_piece_id = musical_piece.id
+  #   if comment.save
+  #     redirect_back(fallback_location: root_path)
+  #   else
+  #     flash[:alert] = "comment can't be blank<br>maximum is 200 characters"
+  #     redirect_back(fallback_location: root_path)
+  #   end
+  # end
 
-  def destroy
-    MusicalPieceComment.find_by(id: params[:id], musical_piece_id: params[:musical_piece_id]).destroy
-    redirect_to musical_piece_path(params[:musical_piece_id])
-  end
+  # def destroy
+  #   MusicalPieceComment.find_by(id: params[:id], musical_piece_id: params[:musical_piece_id]).destroy
+  #   redirect_to musical_piece_path(params[:musical_piece_id])
+  # end
 
   private
 
