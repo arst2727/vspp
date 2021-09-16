3ceda07b4019f865b5c48523a9a8ed8cb3133f43
arst2727 <yanagit4729@gmail.com>
2021-09-16 13:56:57 +0000

    Update CSS

--- app/controllers/member/musical_piece_lists_controller.rb
+++ app/controllers/member/musical_piece_lists_controller.rb
@@ -3,16 +3,16 @@ class Member::MusicalPieceListsController < ApplicationController
   def create
     @musical_piece_list = MusicalPieceList.new(musical_piece_list_params)
     if @musical_piece_list.save
-      redirect_back fallback_location: root_path, success: "マイリストに追加しました"
+      redirect_back fallback_location: root_path, flash: {success: "マイリストに追加しました"}
     else
-      redirect_back fallback_location: root_path, danger: "選択したリストにすでに追加済みか、または他の理由によりリストに追加できませんでした"
+      redirect_back fallback_location: root_path, flash: {danger: "選択したリストにすでに追加済みか、または他の理由によりリストに追加できませんでした"}
     end
   end
 
   def destroy
     @musical_piece_list = MusicalPieceList.find(params[:id])
     @musical_piece_list.destroy
-    redirect_back fallback_location: root_path, success: "マイリストから削除しました"
+    redirect_back fallback_location: root_path, flash: {success: "マイリストから削除しました"}
   end
 
   private
