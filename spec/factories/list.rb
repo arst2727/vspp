65838ca09b37c7c852f1610d345f5858abf7be72
arst2727 <yanagit4729@gmail.com>
2021-10-17 04:31:11 +0000

    Update header-height

--- spec/factories/list.rb
+++ spec/factories/list.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :list do
-    title { Faker::Lorem.characters(number:10) }
-    body { Faker::Lorem.characters(number:30) }
+    name { Faker::Lorem.characters(number:10) }
   end
 end
---
+++ spec/factories/member.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :member do
+    email                 {"test@test"}
+    nickname              {"テストユーザ"}
+    password              {"testtest"}
+    password_confirmation {"testtest"}
+  end
+end
